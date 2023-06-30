335d919b0a75ba356ae645b6e642f97bd7352527
bhavik dhameliya <bhavikdhameliy20@gmail.com>
2023-06-30 09:15:15 +0530

    basic.cpp

--- class/task6.cpp
+++ class/task6.cpp
@@ -10,6 +10,42 @@ class report
     float getavg()
     {
       for(i=0;i<5;i++)
-      total
+      {
+      total=marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
+      per=total/5;
+      return total;
+      return per;
+      }
+    }
+    public:
+    void readinfo()
+    {
+      cout<<"enter admin no:";
+      cin>>adno;
+      cout<<"enter nmae:";
+      cin>>name;
+      for(i=0;i<5;i++)
+    {
+     cout<<"enter marks:";
+     cin>>marks[i];
+    }
+    av=getavg();
+    }
+    void display()
+    {
+      cout<<"admin no:"<<adno<<endl;
+      cout<<"name:"<<name<<endl;
+      for(i=0;i<5;i++)
+      {
+        cout<<"marks:"<<marks[i]<<endl;
+      }
+      cout<<"avra:"<<av<<endl;
+      cout<<"per:"<<per<<endl;
     }
 };
+int main()
+{
+  report r;
+  r.readinfo();
+  r.display();
+}
